756efe93925bf18929980ba5c61d804bed868756
adnrbp <adnrbp@gnd.com>
2016-08-11 20:17:11 -0400

    inicio-guess-game

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern create rand; //new-2
+
+use std::io;
+use std::cmp::Ordering; //new-3
+use rand::Rng;//new-2
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1,101);//new-2
+    println!("the secret number is {}", secret_number); //new-2
+    
+    println!("Please input your guess.");
+    
+    //create string
+    let mut guess = String::new();
+
+    //get number
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
+
+    //new-3
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
