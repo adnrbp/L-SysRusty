00c4a80c793e43cb2e799b4773a348d5f88dbab7
adnrbp <adnrbp@gnd.com>
2016-08-26 21:58:35 -0400

    loop for multiple guesses

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,24 @@ use rand::Rng;//new-2
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);//new-2
-    println!("the secret number is {}", secret_number); //new-2
-    
-    println!("Please input your guess.");
-    
-    //create string
-    let mut guess = String::new();
+    //println!("the secret number is {}", secret_number); //new-2
 
-    //get number
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop{ //new-4
+        println!("Please input your guess.");
+        
+        //create string
+        let mut guess = String::new();
 
-    //new-3.5: string to real-num, shadow prev "guess"
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        //get number
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        //new-3.5: string to real-num, shadow prev "guess"
+        let guess: u32 = match guess.trim().parse() { //new-4.6 eval Result from .parse
+            Ok(num) => num, //new-4.6 Handle error by non-number values
+            Err(_) => continue,
+        };
+        //.expect("Please type a number!");
 
     
     println!("You guessed: {}", guess);
@@ -28,6 +32,10 @@ fn main() {
     match guess.cmp(&secret_number){
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break; //new-4.5
+        }
+    }
     }
 }
